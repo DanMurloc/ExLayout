06707753fc3b59ea9b876286e519ba2f078445d0
DanMurloc <strokalov1@gmail.com>
2024-05-14 22:43:04 +0300

    first commit

---
+++ app/src/main/java/com/example/exploringlayouts/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.exploringlayouts;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linelay);
+    }
+
+    public void loadConstarintLayout(View v){
+        setContentView(R.layout.activity_main);
+    }
+    public void loadTableLayout(View v){
+        setContentView(R.layout.mytable);
+    }
+
+    public void loadMenuLayout(View v){
+        setContentView(R.layout.linelay);
+    }
+}
